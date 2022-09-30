67fd46a6983fd9151abcadd2b418466d56f7a57d
Angel123Lara <adjls@gmail.com>
2022-09-30 00:20:53 -0500

    DB connected

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const { Sequelize } = require("sequelize");
+const express = require('express');
+const sequelize = require('./config/db');
 
-const Sequelize = new Sequelize(
-    'd8ovmcqvs357ps',
-    'vsjzsncyduaglx:',
-    '97b577fa35e5b13ad0407973e5907e984a1bd1501fb6e1ad40f4bde160353980', 
-    {
-        host: 'ec2-54-164-40-66.compute-1.amazonaws.com',
-        dialect: 'postgres',
-        native: true,
-        ssl: true
-    }
-);
+const app = express();
+
+try {
+    sequelize.authenticate();
+    sequelize.sync();
+    console.log('Connected to DB');
+} catch (error) {
+    console.log('Unable to connect to DB:', error);
+}
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("Server listing on PORT 3000");
+});
+
+
+
---
+++ config/db.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+    'd8ovmcqvs357ps',
+    'vsjzsncyduaglx:',
+    '97b577fa35e5b13ad0407973e5907e984a1bd1501fb6e1ad40f4bde160353980', 
+    {
+        host: 'ec2-54-164-40-66.compute-1.amazonaws.com',
+        dialect: 'postgres',
+        native: true,
+        ssl: true
+    }
+);
+
+module.exports = Sequelize;
