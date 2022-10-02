4af0001e98dde7651d013069c16940ab949ff069
Angel123Lara <adjls@gmail.com>
2022-10-01 21:59:46 -0500

    Fix error deploy heroku t2

--- controllers/users.js
+++ controllers/users.js
@@ -15,8 +15,13 @@ async function getUser(req, res){
 }
 
 async function getUsers(req, res){
+    try{
     const users = await User.findAll();
     res.status(200).json(users);
+    }
+    catch {
+        res.json({'info': 'debug'})
+    }
 }
 
 async function updateUser(req, res){
