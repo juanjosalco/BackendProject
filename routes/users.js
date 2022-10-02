f3d7ae80fa47f427df34881b849ba73893ecb874
Juan José Salazar Cortés <juansalazar@Juans-MacBook-Air-4.local>
2022-10-02 16:38:39 -0500

    Cuarto intento de traer por rol

--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,11 @@ const {
 
 } = require('../controllers/users');
 
-router.get('/', getUsers);
+// router.get('/', getUsers);
 router.get('/:id', getUser);
 router.post('/', createUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
-router.get('/rol', bringByRol);
+router.get('/', bringByRol);
 
 module.exports = router;
