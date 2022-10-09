cbc0f99e2a0251d4c24a8de35a95378fbf10da5e
Angel123Lara <adjls@gmail.com>
2022-10-09 03:05:50 -0500

    include added in controllers

--- controllers/book.js
+++ controllers/book.js
@@ -1,5 +1,7 @@
 const {Sequelize} = require('sequelize');
 const book = require('../models/book');
+const Category =  require('../models/category') 
+const Editorial = require('../models/editorial')
 
 function createBook (req,res) {
     const body = req.body;
@@ -15,13 +17,20 @@ async function getBook(req,res){
         
         return (res.status(400).json({error : "Try with numeric value"}))
     }
-    const bk = await book.findByPk(id);
+    const bk = await book.findByPk(id,
+        {
+            include: [{association: book.hasOne(Category),
+                association: book.hasOne(Editorial)}]
+        });
     res.status(200).json(bk);
 }
 
 async function getBooks(req,res){
     try{
-        const books = await book.findAll();
+        const books = await book.findAll({
+            include: [{association: book.hasOne(Category),
+                association: book.hasOne(Editorial)}]
+        });
         res.status(200).json(books)
     }
     catch (err){
