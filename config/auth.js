94e640f577d43c165a44f70ea8afeff5d063c8aa
Miguel Chavez <toonchavez8@gmail.com>
2022-10-30 09:40:50 -0600

    update auth with prettier as a test
    testing deploy from this branch on railway so updated styling on auth.js file

--- config/auth.js
+++ config/auth.js
@@ -1,45 +1,46 @@
-const secret = require('./secret');
-const {expressjwt} = require('express-jwt');
+const secret = require("./secret");
+const { expressjwt } = require("express-jwt");
 
 function getTokenFromHeader(req) {
-    if (req.headers.authorization && req.headers.authorization.split(' ')[0] == 'Bearer') {
-        return req.headers.authorization.split(' ')[1]
-    }
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.split(" ")[0] == "Bearer"
+	) {
+		return req.headers.authorization.split(" ")[1];
+	}
 }
 
 const auth = {
-    required: function(req,res,next){
-        if(!req.auth) {
-            
-            return res.status(401).json({"Error" :"Credentials not founded"});
-        }
-        if (!req.user || !req.auth.user){
-            console.log ('NOT pass double verification')
-            return res.status(403).json({Error : "Something wrong in verification"});
-        }
-        
-        next();
-    }
-    ,
-    isAdmin: function (req, res, next) {
-        
-        if(!req.auth) {
-            
-            return res.status(401).json({"Error" :"Credentials not founded"});
-        }
-        if (req.auth.role !== 'admin') {
-            return res.status(403).json({"Error":"You are not an admin, can't access this info"});
-        }
-        //console.log(req.auth)
-        next();
-    },
-    optional: expressjwt({
-        secret: secret,
-        algorithms: ['HS256'],
-        userProperty: 'user',
-        credentialsRequired: false,
-        getToken: getTokenFromHeader
-    })
-}
+	required: function (req, res, next) {
+		if (!req.auth) {
+			return res.status(401).json({ Error: "Credentials not founded" });
+		}
+		if (!req.user || !req.auth.user) {
+			console.log("NOT pass double verification");
+			return res.status(403).json({ Error: "Something wrong in verification" });
+		}
+
+		next();
+	},
+	isAdmin: function (req, res, next) {
+		if (!req.auth) {
+			return res.status(401).json({ Error: "Credentials not founded" });
+		}
+		if (req.auth.role !== "admin") {
+			return res
+				.status(403)
+				.json({ Error: "You are not an admin, can't access this info" });
+		}
+		//console.log(req.auth)
+		next();
+	},
+	optional: expressjwt({
+		secret: secret,
+		algorithms: ["HS256"],
+		userProperty: "user",
+		credentialsRequired: false,
+		getToken: getTokenFromHeader,
+	}),
+};
 
 module.exports = auth;
