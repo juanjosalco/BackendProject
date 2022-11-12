6c4c168954c21dda9127d755dcea8fa8f7159f0c
Miguel Chavez <toonchavez8@gmail.com>
2022-11-12 12:17:07 -0600

    update swagger with users and add auth seperation

--- config/auth.js
+++ config/auth.js
@@ -13,25 +13,63 @@ function getTokenFromHeader(req) {
 
 // Verify Token
 const auth = {
+	// if the user is logged in
 	required: function (req, res, next) {
 		if (!req.auth) {
-			return res.status(401).json({ Error: "Credentials not founded" });
+			return res
+				.status(401)
+				.json({ Error: "Hi please login to access this info" });
 		}
-		if (!req.user || !req.auth.user) {
-			return res.status(403).json({ Error: "Something wrong in verification" });
+		// if admin
+		if (req.auth.role == 1) {
+			return next();
+		}
+		// if not requested user
+		if (req.auth.id != req.params.id) {
+			return res.status(401).json({
+				// send auth id and params id
+				Error: "You are not authorized to access this info",
+			});
 		}
 		next();
 	},
 	isAdmin: function (req, res, next) {
 		if (!req.auth) {
-			return res.status(401).json({ Error: "Credentials not founded" });
+			return res
+				.status(401)
+				.json({ Error: "Hi please login to access this info" });
 		}
 		if (req.auth.role !== 1) {
 			return res
 				.status(403)
 				.json({ Error: "You are not an admin, can't access this info" });
 		}
-		//console.log(req.auth)
+		next();
+	},
+	isEditor: function (req, res, next) {
+		if (!req.auth) {
+			return res
+				.status(401)
+				.json({ Error: "Hi please login to access this info" });
+		}
+		if (req.auth.role !== 3 || req.auth.role) {
+			return res
+				.status(403)
+				.json({ Error: "You are not an editor, can't access this info" });
+		}
+		next();
+	},
+	isUser: function (req, res, next) {
+		if (!req.auth) {
+			return res
+				.status(401)
+				.json({ Error: "Hi please login to access this info" });
+		}
+		if (req.auth.role !== 2 || req.auth.role) {
+			return res
+				.status(403)
+				.json({ Error: "You are not an user, can't access this info" });
+		}
 		next();
 	},
 	optional: expressjwt({
